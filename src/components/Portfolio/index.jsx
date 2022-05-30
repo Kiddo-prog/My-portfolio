09f9f4fb558fc570a5622d63f3acf4d5f7a770db
Kiddo-prog <husseinmubarak500@gmail.com>
2022-05-30 20:07:36 +0100

    Modified changes

--- src/components/Portfolio/index.jsx
+++ src/components/Portfolio/index.jsx
@@ -13,6 +13,11 @@ import {
 import { ExternalLinkIcon } from "@chakra-ui/icons";
 import { FaGithub } from 'react-icons/fa'
 import Reveal  from "react-reveal/Reveal";
+import { useRef, useState } from "react";
+import { useSpring, animated } from "@react-spring/web";
+import { useControls } from "leva";
+import "./styles.css";
+
 
 export default function PortfolioComponent() {
   const bg = useColorModeValue("rgb(2,0,36)", "#fff");
@@ -60,18 +65,19 @@ export default function PortfolioComponent() {
             <Heading
               textAlign="center"
               fontSize={["15px", "20px", "25px", "27px"]}
-              marginTop="0.5em"
+              marginTop="1em"
             >
               {item.title}
+              {item.bash}
             </Heading>
-            <Text
+            {/* <Text
               w={["100%", null, "100%"]}
               margin="1em auto"
               letterSpacing={1}
             >
               {item.description}
-            </Text>
-            <Flex justifyContent={'space-between'} alignItems={'center'} columnGap={'1em'}>
+            </Text> */}
+            <Flex justifyContent={'space-between'} alignItems={'center'} columnGap={'1em'} mt={'2em'}>
               <Button
                 as={"a"}
                 target={'_blank'}
@@ -99,6 +105,46 @@ export default function PortfolioComponent() {
         ))}
         </Reveal>
       </SimpleGrid>
+      {/* <Card /> */}
     </>
+  );
+}
+
+
+
+const calc = (x, y, rect) => [
+  -(y - rect.top - rect.height / 2) / 5,
+  (x - rect.left - rect.width / 2) / 5,
+  1.4
+];
+const trans = (x, y, s) =>
+  `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`;
+
+export function Card() {
+  const ref = useRef(null);
+  const [xys, set] = useState([0, 0, 1]);
+  const config = useControls({
+    mass: 1,
+    tension: 170,
+    friction: 26,
+    clamp: false,
+    precision: 0.01,
+    velocity: 0,
+    easing: (t) => t
+  });
+  const props = useSpring({ xys, config });
+
+  return (
+    <div className="ccard-main" ref={ref}>
+      <animated.div
+        className="ccard"
+        style={{ transform: props.xys.to(trans) }}
+        onMouseLeave={() => set([0, 0, 1])}
+        onMouseMove={(e) => {
+          const rect = ref.current.getBoundingClientRect();
+          set(calc(e.clientX, e.clientY, rect));
+        }}
+      />
+    </div>
   );
 }
