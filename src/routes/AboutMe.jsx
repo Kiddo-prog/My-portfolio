73fec228785c164eaf8a5d3ad5d113a512ae0031
Kiddo-prog <husseinmubarak500@gmail.com>
2022-06-11 18:00:47 +0100

    Modified & made new changes

--- src/routes/AboutMe.jsx
+++ src/routes/AboutMe.jsx
@@ -1,8 +1,26 @@
 import React from "react";
-import { Box, Text, Heading } from "@chakra-ui/react";
-import { useTypewriter } from 'react-simple-typewriter'
+import { 
+  Box, 
+  Text, 
+  Heading,
+  Stack,
+  HStack
+} from "@chakra-ui/react";
+import { useTypewriter } from 'react-simple-typewriter';
+import { VideoScroll } from 'react-video-scroll'
+
 
 export default function AboutMe() {
+
+  const setStyles = (wrapperEl, videoEl, playbackRate) => {
+    wrapperEl.style.marginTop = `calc(180% - ${Math.floor(videoEl.duration) *
+      playbackRate +
+      'px'})`
+    wrapperEl.style.marginBottom = `calc(180% - ${Math.floor(videoEl.duration) *
+      playbackRate +
+      'px'})`
+  }
+
   const {text} = useTypewriter({
     words: ['Richer', 'Smarter', 'Happier'],
     loop: 0,
@@ -14,21 +32,48 @@ export default function AboutMe() {
     <Box my={["3em", "4em"]} mx={["2em", "3em"]}>
       <Heading
         fontSize={["18px", "20px", "24px", "35px"]}
-        mb="1em"
-        fontStyle="italic"
-        ml="1em"
+        mb="0.5em"
+        fontFamily={'"Public Sans", sans-serif'}
+        _after={{content: '""', display: 'block', bgColor: 'black', width: '6%', height: '5px', mt: 2}}
       >
         About me
       </Heading>
-      <Text fontSize={["14", "16", "19", "22"]} fontWeight="400" fontStyle={'italic'}>
-        Fullstack developer with a passion for developing scalable web
-        applications. Growing my skillset, great
-        collaboration with teams while contributing to the positive outcome of
-        making people{" "}
-        <b>
-          <i>{text}</i>
-        </b>
-      </Text>
+      <Stack mt={20}>
+        <HStack 
+          justifyContent={'space-between'} 
+          flexWrap={{base: 'wrap', md: 'nowrap', lg: 'nowrap'}} 
+          alignItems={'flex-start'} 
+          spacing={10}
+          my={{base: 5, md: 10, lg: 10}}
+          >
+          <Box>
+          <VideoScroll
+            onLoad={props =>
+              setStyles(props.wrapperEl, props.videoEl, props.playbackRate)
+            }
+            playbackRate={15}
+            style={{ position: 'sticky' }}
+          >
+            <video
+              tabIndex="0"
+              autobuffer="autobuffer"
+              preload="preload"
+              style={{ width: '100%', objectFit: 'cover' }}
+              playsInline
+            >
+              <source type="video/mp4" src="/about-me.mp4" />
+            </video>
+          </VideoScroll>
+          </Box>
+            <Text fontSize={["14", "16", "19", "20"]} fontWeight="400" fontFamily={'"Public Sans", sans-serif'}>
+            I'm a Fullstack developer with a passion for developing scalable web
+            applications. Growing my skillset, great
+            collaboration with teams while contributing to the positive outcome of
+            making people{" "}
+            {/* <b>{text}</b> */}
+          </Text>
+        </HStack>
+      </Stack>
     </Box>
   );
 }
