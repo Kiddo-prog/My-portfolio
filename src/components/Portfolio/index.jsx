b0803939fe94c2970dd1c1195d208922dc04cea6
Kiddo-prog <husseinmubarak500@gmail.com>
2022-06-11 18:01:23 +0100

    Made new changes

--- src/components/Portfolio/index.jsx
+++ src/components/Portfolio/index.jsx
@@ -2,36 +2,22 @@ import React from "react";
 import data from "../../data.json";
 import {
   Box,
-  Text,
-  SimpleGrid,
   Image,
   Button,
   Heading,
-  useColorModeValue,
-  Flex
+  Spacer,
+  Flex,
+  Text,
+  Stack,
+  HStack
 } from "@chakra-ui/react";
 import { ExternalLinkIcon } from "@chakra-ui/icons";
 import { FaGithub } from 'react-icons/fa'
-import Reveal  from "react-reveal/Reveal";
-import { useRef, useState } from "react";
-import { useSpring, animated } from "@react-spring/web";
-import { useControls } from "leva";
-import "./styles.css";
 
 
 export default function PortfolioComponent() {
-  const bg = useColorModeValue("rgb(2,0,36)", "#fff");
-  const color = useColorModeValue("#fff", "rgb(2,0,36)");
-  const bgColor = useColorModeValue("#fff", "rgb(2,0,36)");
-  const boxShadow = useColorModeValue(
-    "2px 5px 10px rgb(2,0,36)",
-    "2px 9px 15px #fff"
-  );
   const items = data.projects;
-  const styleLink = {
-    margin: "auto",
-    width: "100%",
-  };
+
   return (
     <>
       <Heading
@@ -42,7 +28,36 @@ export default function PortfolioComponent() {
       >
         Portfolio
       </Heading>
-      <SimpleGrid
+      <Box marginX={10}>  
+          {
+            items.map(item => (
+              <Flex key={item.id} alignItems={'flex-start'} flexDirection={{base: 'column', md: 'row', lg:'row'}} justifyContent={'space-around'}>
+                <Box flex="5" mb={10}>
+                  <Image src={item.image} alt="portfolio" objectFit="contain" />
+                </Box>
+              <Spacer />
+              <Box flex="5" mb={10}>
+                <Heading fontFamily={'"Public Sans", sans-serif'} mb={5}>{item.title}</Heading>
+                <Text fontFamily={'"Public Sans", sans-serif'}>{item.description}</Text>
+                <Box marginY={5}>
+                  {
+                    item.tools.map((tool, index) => (
+                      <Button key={index} mr={2} bgColor={'blue.600'} color={'white'} _hover={{bgColor: 'blue.800'}} disabled>{tool}</Button>
+                    ))
+                  }
+                </Box>
+                <Stack mt={5}>
+                  <HStack>
+                    <Button as="a" href={item.link} bgColor={'blue.600'} color={'white'} _hover={{bgColor: 'blue.800'}} rightIcon={<ExternalLinkIcon />}>Live</Button>
+                    <Button as="a" href={item.github} rightIcon={<FaGithub />}>Code</Button>
+                  </HStack>
+                </Stack>
+              </Box>
+              </Flex>
+            ))
+          }
+      </Box>
+      {/* <SimpleGrid
         columns={[1, null, 2, 3]}
         spacing="40px"
         padding={5}
@@ -70,13 +85,6 @@ export default function PortfolioComponent() {
               {item.title}
               {item.bash}
             </Heading>
-            {/* <Text
-              w={["100%", null, "100%"]}
-              margin="1em auto"
-              letterSpacing={1}
-            >
-              {item.description}
-            </Text> */}
             <Flex justifyContent={'space-between'} alignItems={'center'} columnGap={'1em'} mt={'2em'}>
               <Button
                 as={"a"}
@@ -104,47 +112,7 @@ export default function PortfolioComponent() {
           </Box>
         ))}
         </Reveal>
-      </SimpleGrid>
-      {/* <Card /> */}
+      </SimpleGrid> */}
     </>
-  );
-}
-
-
-
-const calc = (x, y, rect) => [
-  -(y - rect.top - rect.height / 2) / 5,
-  (x - rect.left - rect.width / 2) / 5,
-  1.4
-];
-const trans = (x, y, s) =>
-  `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`;
-
-export function Card() {
-  const ref = useRef(null);
-  const [xys, set] = useState([0, 0, 1]);
-  const config = useControls({
-    mass: 1,
-    tension: 170,
-    friction: 26,
-    clamp: false,
-    precision: 0.01,
-    velocity: 0,
-    easing: (t) => t
-  });
-  const props = useSpring({ xys, config });
-
-  return (
-    <div className="ccard-main" ref={ref}>
-      <animated.div
-        className="ccard"
-        style={{ transform: props.xys.to(trans) }}
-        onMouseLeave={() => set([0, 0, 1])}
-        onMouseMove={(e) => {
-          const rect = ref.current.getBoundingClientRect();
-          set(calc(e.clientX, e.clientY, rect));
-        }}
-      />
-    </div>
   );
 }
