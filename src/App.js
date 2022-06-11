b6d0e785ef42d7e825fd22108685a90a2c590d55
Kiddo-prog <husseinmubarak500@gmail.com>
2022-06-11 17:57:38 +0100

    Added react router

--- src/App.js
+++ src/App.js
@@ -2,15 +2,31 @@ import React from "react";
 import "./App.css";
 import About from "./routes/About.jsx";
 import Main from "./components/Main.jsx";
-import { Outlet } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+
+import AboutMe from "./routes/AboutMe";
+import Portfolio from "./routes/Portfolio";
+import Services from "./routes/Services";
+import Testimonial from "./routes/Testimonial";
+import Navigation from "./components/Header";
+import Footer from "./components/Footer";
+
+
 
 function App() {
   return (
-    <div>
+    <>
+      <Navigation />
       <About />
-      <Outlet />
       <Main />
-    </div>
+        <Routes>
+            <Route path="/about" element={<AboutMe />} />
+            <Route path="/portfolio" element={<Portfolio />} />
+            <Route path="/service" element={<Services />} />
+          <Route path="/testimonial" element={<Testimonial />} />
+        </Routes>
+        <Footer />
+    </>
   );
 }
 
