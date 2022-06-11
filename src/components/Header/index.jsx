181cdbff3c9c39133b00f7bf66a64f7c9bf592d9
Kiddo-prog <husseinmubarak500@gmail.com>
2022-06-11 18:03:22 +0100

    Added darkmode icon & made new changes

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,36 +1,35 @@
 import React from "react";
-import { Flex, Spacer, Heading, Box } from "@chakra-ui/react";
+import { Flex, Heading, Box, Image } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import { useColorModeValue } from "@chakra-ui/react";
+import DarkMode from "../utils/DarkMode";
 
-import MobileMenu from "../Menu";
+import MobileMenu from '../SideMenu'
 
 export default function Navigation({ mode }) {
-  const bg = useColorModeValue("rgb(2,0,36)", "#fff");
-  const color = useColorModeValue("#fff", "rgb(2,0,36)");
-  const boxShadow = useColorModeValue(
-    "2px 5px 10px rgb(2,0,36)",
-    "2px 5px 10px #fff"
-  );
+  // const bg = useColorModeValue("rgb(2,0,36)", "#fff");
+  const color = useColorModeValue("rgb(2,0,36)",  "#fff");
   const link = {
     letterSpacing: "1.5px",
     marginLeft: "1em",
     fontSize: "1.25em",
+    marginRight: "1em"
   };
 
   return (
-    <Box p={5} bgColor={bg} color={color} boxShadow={boxShadow}>
-      <Flex alignItems="center">
+    <Box p={5} color={color} position={'sticky'}>
+      <Flex alignItems="center" justifyContent={'space-between'}>
         <Box>
           <Heading
             fontSize={["16", "18", "21", "23"]}
             letterSpacing="2px"
             display="flex"
           >
-            <Link to="/">Codewithmubi</Link>
+            <Link to="/">
+              <Image src={'/images/logo.png'} alt="logo" />
+            </Link>
           </Heading>
         </Box>
-        <Spacer />
         <Box
           fontSize={["9", "10", "11", "13"]}
           display={{ base: "none", md: "block", lg: "block" }}
@@ -39,14 +38,13 @@ export default function Navigation({ mode }) {
             About
           </Link>
           <Link to="/portfolio" style={link}>
-            Portfolio
+            Projects
           </Link>
-          <Link to="/service" style={link}>
-            Services
+
+          <Link to="/contact" style={link}>
+            Contact
           </Link>
-          {/* <Link to="/testimonial" style={link}>
-            Testimonial
-          </Link> */}
+          <DarkMode />
         </Box>
         <MobileMenu />
       </Flex>
