a561652c050cb80818cbace3319230c02bb26b28
Kiddo-prog <husseinmubarak500@gmail.com>
2022-06-12 08:58:14 +0100

    Modified styling

--- src/components/Portfolio/index.jsx
+++ src/components/Portfolio/index.jsx
@@ -14,9 +14,9 @@ import {
 import { ExternalLinkIcon } from "@chakra-ui/icons";
 import { FaGithub } from 'react-icons/fa'
 
-
 export default function PortfolioComponent() {
   const items = data.projects;
+  
 
   return (
     <>
@@ -32,17 +32,18 @@ export default function PortfolioComponent() {
           {
             items.map(item => (
               <Flex key={item.id} alignItems={'flex-start'} flexDirection={{base: 'column', md: 'row', lg:'row'}} justifyContent={'space-around'}>
-                <Box flex="5" mb={10}>
+
+                <Box flex="5" mb={10} data-aos="fade-right">
                   <Image src={item.image} alt="portfolio" objectFit="contain" />
                 </Box>
               <Spacer />
-              <Box flex="5" mb={10}>
+              <Box flex="5" mb={10} data-aos="fade-left">
                 <Heading fontFamily={'"Public Sans", sans-serif'} mb={5}>{item.title}</Heading>
                 <Text fontFamily={'"Public Sans", sans-serif'}>{item.description}</Text>
                 <Box marginY={5}>
                   {
                     item.tools.map((tool, index) => (
-                      <Button key={index} mr={2} bgColor={'blue.600'} color={'white'} _hover={{bgColor: 'blue.800'}} disabled>{tool}</Button>
+                      <Button key={index} mr={2} bgColor={'blue.600'} mt={3} color={'white'} _hover={{bgColor: 'blue.800'}} disabled>{tool}</Button>
                     ))
                   }
                 </Box>
