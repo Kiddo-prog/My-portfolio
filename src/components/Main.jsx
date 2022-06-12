7b9f55a34a611a0c43b5b8fed0592d3464f892db
Kiddo-prog <husseinmubarak500@gmail.com>
2022-06-12 08:57:51 +0100

    Added new components

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Contact from "./Contact";
 import Skill from "./Skill";
+import Portfolio from "./Portfolio"
+import AboutMe from "../routes/AboutMe";
 
 function Main() {
   return (
     <>
-      <Skill />
-      <Contact />
+        <AboutMe />
+        <Portfolio />
+        <Skill />
+        <Contact />
     </>
   );
 }
