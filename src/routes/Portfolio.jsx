ec5f3e51cab4a7e5b2bcbc92e40c723ba2c264c8
Kiddo-prog <husseinmubarak500@gmail.com>
2022-06-12 11:21:13 +0100

    Changed link to Hashlink

--- src/routes/Portfolio.jsx
+++ src/routes/Portfolio.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import PortfolioComponent from "../components/Portfolio";
 
+import { Box } from "@chakra-ui/react";
+
 export default function Portfolio() {
   return (
     <>
+    <Box id="portfolio">
       <PortfolioComponent />
+    </Box>
     </>
   );
 }
