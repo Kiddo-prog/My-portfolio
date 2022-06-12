e588f1174727f31fb0459d60025fd950db8299f7
Kiddo-prog <husseinmubarak500@gmail.com>
2022-06-12 08:55:31 +0100

    Added Routes, Route & updated components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import About from "./routes/About.jsx";
 import Main from "./components/Main.jsx";
@@ -11,9 +11,17 @@ import Testimonial from "./routes/Testimonial";
 import Navigation from "./components/Header";
 import Footer from "./components/Footer";
 
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+
 
 
 function App() {
+
+  useEffect(() => {
+    AOS.init()
+  })
   return (
     <>
       <Navigation />
